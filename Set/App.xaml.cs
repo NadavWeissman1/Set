9c4b84c1adbbce93fbe7760d15c095a37a74658f
ASUS <ASUS@DESKTOP-EQO9FQA>
2025-09-14 11:50:31 +0300

    Add project files.

---
+++ Set/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Set
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Set/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Set
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
